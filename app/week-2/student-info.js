fae8a12ef7bf72cbbf7e9b6c15ddc1ec76d60323
SamarBeer <singhsamarbeer@gmail.com>
2025-05-26 09:57:56 -0600

    Create student-info page

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function Page() {
+  return (
+    <main>
+      <h1>Student Information</h1>
+      <Link href="https://github.com/SamarBeer/cprg306-assignments" passHref>
+        <a target="_blank" rel="noopener noreferrer">GitHub Repository</a>
+      </Link>
+    </main>
+  );
+}
