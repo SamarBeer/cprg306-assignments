38e88fe1cb8364c7f87997ec028c70c5a3086988
SamarBeer <singhsamarbeer@gmail.com>
2025-05-26 22:36:39 -0600

    udated student information (added my student id)

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,9 @@ import Link from 'next/link';
 export default function StudentInfo() {
   return (
     <div>
-      <h1>Student Information</h1>
+      <h1>Samarbeer Singh <br />
+      Student ID: 000938609
+      </h1>
       <Link href="https://github.com/SamarBeer/cprg306-assignments" legacyBehavior>
         <a target="_blank" rel="noopener noreferrer">GitHub Repository</a>
       </Link>
