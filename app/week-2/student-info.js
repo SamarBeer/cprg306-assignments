08c07140daf6a6765769434f52150eec914d86c1
SamarBeer <singhsamarbeer@gmail.com>
2025-05-26 22:34:04 -0600

    done some changes in sudent info component

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 export default function StudentInfo() {
   return (
-    <main>
+    <div>
       <h1>Student Information</h1>
-      <Link href="https://github.com/SamarBeer/cprg306-assignments" passHref>
+      <Link href="https://github.com/SamarBeer/cprg306-assignments" legacyBehavior>
         <a target="_blank" rel="noopener noreferrer">GitHub Repository</a>
       </Link>
-    </main>
+    </div>
   );
 }
+
+
